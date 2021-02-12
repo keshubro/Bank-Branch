43ed046d527c4392c0ccd390fc2de4d6f421a283
Preti15 <74337234+Preti15@users.noreply.github.com>
2021-02-12 09:08:47 +0530

    Add login authentication

---
+++ server.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+
+
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+
+const userdb = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'UTF-8'));
+
+server.use(jsonServer.defaults());
+
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json())
+
+// Check if the user exists in database
+function isAuthenticated({username, password}){
+    console.log(userdb.customers);
+    return userdb.customers.findIndex(user => user.username === username && user.password === password) !== -1
+    
+  }
+
+server.post('/auth/login', (req, res) => {
+    console.log(req.body);
+    const {username, password} = req.body
+    if (!isAuthenticated( {username, password} ) ) {
+      const status = 401;
+      const message = 'Incorrect Username or Password';
+      res.status(status).json({status, message});
+      return;
+    }
+    res.status(200).send("OK");
+})
+
+
+
+server.use(router)
+
+server.listen(3000, () => {
+  console.log('Backend Server Started')
+})
