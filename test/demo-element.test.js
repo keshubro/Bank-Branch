49129337f57a202e104f0bd32d4ae7ee41e5c0eb
Preti15 <74337234+Preti15@users.noreply.github.com>
2021-02-17 13:49:09 +0530

    Add commented demo element test js same as that of main

--- test/demo-element.test.js
+++ test/demo-element.test.js
@@ -1,19 +1,19 @@
-/*import { html, fixture, expect } from '@open-wc/testing';
+// import { html, fixture, expect } from '@open-wc/testing';
 
-//import '../src/demo-element';
+// import '../src/demo-element';
 
-//describe('<demo-element/>', () => {
-  //let el;
+// describe('<demo-element/>', () => {
+//   let el;
 
-  //beforeEach(async () => {
- --   el = await fixture(html`
-      <demo-element></demo-element>
-    `);
-  });
+//   beforeEach(async () => {
+//     el = await fixture(html`
+//       <demo-element></demo-element>
+//     `);
+//   });
 
-  it('Component has X properties initialized', () => {
-    expect(el.title).to.be.an('string');
+//   it('Component has X properties initialized', () => {
+//     expect(el.title).to.be.an('string');
 
-    expect(el.title).to.equal('LitElement');
-  });
-}); */
+//     expect(el.title).to.equal('LitElement');
+//   });
+// });
