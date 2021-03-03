ed7f73055f95ddffce2c1bdef8d84e3c839f3cc6
Padmashani <padmashani98@gmail.com>
2021-03-03 19:56:18 +0530

    Addded background image to the login page

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -1,5 +1,5 @@
+import { LitElement, html, css } from "lit-element";
 
-import { LitElement, html, css } from 'lit-element';
 import '@lion/form/lion-form.js';
 import '@lion/input/lion-input.js';
 import '@lion/button/lion-button.js';
@@ -9,10 +9,8 @@ import {  Required } from '@lion/form-core';
 import { localize,LocalizeMixin } from '@lion/localize';
 import { ajax } from '@lion/ajax';
 
-
-
-export class LoginComponent extends LocalizeMixin(LitElement){
-
+class LoginComponent extends LocalizeMixin(LitElement){
+    
     static get localizeNamespaces() {
         return [
           { 'lit-html-example': locale => import(`../translations/${locale}.js`) },
@@ -20,187 +18,193 @@ export class LoginComponent extends LocalizeMixin(LitElement){
         ];
     }
 
-    static get styles() {
-        return css`
-            .loginbg{
-                height: 800px;
-                width: 500px;
-                background: url("./images/dummy-logo.png");
-                /* background-color: blue;  */
-                padding: 50px;
-                
-            }
-            .login-container{
-                margin: auto;
-                width:350px;
-            }
-            .cont{
-                //width: 500px;
-                //background-color: lightblue;
-                //box-shadow: 5px 10px 18px #888888;
-               // border-radius: 5px;
-               //margin-top: 150px;
-                height: 100%;
-            }
-            
-            lion-input{
-                padding-top: 15px;
-            }
-
-
-            input{
-                border-radius: 5px;
-            }
-
-            button{
-                margin: 0px;
-                font-size: 14px;
-                height:30px;
-                border: 1px solid #fff; 
-                background-color: transparent;
-            }
-
-            lion-button{
-                background-color: green;
-                color:white;
-                border-radius:5px;
-            }
-
-            lion-button:hover {
-                background-color: #057305c2;
-                cursor:pointer;
-            }
-
-            .login{
-                
-                text-align: right;
-                padding: 15px;
-                font-size: 16px;
-                font-weight: bold;
-                outline: none; 
-            }
-
-            .form-control{
-                padding: 0 0 0 5px;
-            }
-            
-            h2{
-                text-align: center;
-            }
-
-            a{
-                text-decoration: none;
-            }
-
-            #errormessage{
-                color: red;
-            }
-
-            .input-label-prefix{
-                display: block;
-                margin-right: 8px;
-            }
-
-            .input-label{
-                display: none;
-            }
-
-            .bgImg{
-                height: 800px;
-                width: 2024px;
-                background: url("./images/bg.jpg") no-repeat center center fixed;
-                /* background-color: blue;  */
-                padding: 5px;
-                background-size: cover;
-            }
-
-            @media only screen and (max-width: 576px) {
-                .input-label{
-                    display: inline-block;
-                }
-                .input-label-prefix{
-                    display: none;
-                }
-
-                .input-label-suffix{
-                    display: block;
-                }
-            }
-
-            @media only screen and (min-width: 700px) {
-                .login-container{
-                    margin-top: 250px;
-                    width:550px;
-                }
-
-            }
-
-            @media only screen and (min-height: 900px) {
-                .login-container{
-                    margin-top: 350px;
-                    width:650px;
-                }
-
-            }
-
-
-
-        `
-
-    }
-
-    constructor(){
+    constructor() {
         super();
         this.errorMessage = null;
         loadDefaultFeedbackMessages();
     }
 
+  static get styles(){
+      return css`
 
-    static get properties() {
-        return{
-            errorMessage: { type: String }
-        };
-    }
+        .big-image {
+            height: 94vh;
+            width: 100vw;
+            position: relative;
+            background-size: cover;
+            background-position: 50% 50%;
+            //background-image: url("https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=80");
+            background-image: url("./images/bg.jpg");
+          }
+          
+          .overlay {
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            top: 0;
+            left: 0;
+            background: rgba(0, 0, 0, 0.65);
+            color: white;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+          }
+          
+          .overlay h1 {
+            font-size: 65px;
+            letter-spacing: 0px;
+            margin: 0 0 5px;
+          }
+          
+          .overlay p {
+            margin: 0;
+            font-size: 28px;
+          }
 
-        
-    authenticateUser(ev){
-
-        ev.preventDefault()
-
-        const formData = ev.target.serializedValue;
-        
-        const url = 'http://localhost:3000/auth/login';
-
-        const username = this.shadowRoot.getElementById('username').value;
-        const password = this.shadowRoot.getElementById('password').value;
-
-        
-        
-        if(username && password){
-
-            ajax
-            .post(url,formData)
-            
-            .then(response => {
-                this.errorMessage = null;
-                sessionStorage.setItem("username", username);
-                window.location.href = "/search";
-            })
-            .catch(error => {
-                this.errorMessage = "Invalid Username or Password";
-            });
-        }else{
-           console.log("field is empty"); 
+        //   .login-container{
+        //     margin: 130px;
+        //     width:350px;
+        // }
+        .cont{
+            //width: 500px;
+            //background-color: lightblue;
+            //box-shadow: 5px 10px 18px #888888;
+           // border-radius: 5px;
+           //margin-top: 150px;
+           // height: 100%;
         }
-         
-    }
-
-    render() {
         
-        return html`
-        <link  rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
-           <div class="bgIm">
-                <lion-form class="login-container" @submit=${this.authenticateUser} responsive>
+        lion-input{
+            padding-top: 15px;
+        }
+
+
+        input{
+            border-radius: 5px;
+        }
+
+        button{
+            margin: 0px;
+            font-size: 14px;
+            height:30px;
+            border: 1px solid #fff; 
+            background-color: transparent;
+        }
+
+        lion-button{
+            background-color: green;
+            color:white;
+            border-radius:5px;
+        }
+
+        lion-button:hover {
+            background-color: #057305c2;
+            cursor:pointer;
+        }
+
+        .login{
+            
+            text-align: right;
+            padding: 15px;
+            font-size: 16px;
+            font-weight: bold;
+            outline: none; 
+        }
+
+        .form-control{
+            padding: 0 0 0 5px;
+        }
+        
+        h2{
+            text-align: center;
+        }
+
+        a{
+            text-decoration: none;
+        }
+
+        #errormessage{
+            color: red;
+        }
+
+        .input-label-prefix{
+            display: block;
+            margin-right: 8px;
+        }
+
+        .input-label{
+            display: none;
+        }
+
+        
+
+        @media only screen and (max-width: 576px) {
+            .input-label{
+                display: inline-block;
+            }
+            .input-label-prefix{
+                display: none;
+            }
+
+            .input-label-suffix{
+                display: block;
+            }
+        }
+
+          
+        
+
+      `;
+  }
+
+  static get properties() {
+    return{
+        errorMessage: { type: String }
+    };
+}
+
+    
+authenticateUser(ev){
+
+    ev.preventDefault()
+
+    const formData = ev.target.serializedValue;
+    
+    const url = 'http://localhost:3000/auth/login';
+
+    const username = this.shadowRoot.getElementById('username').value;
+    const password = this.shadowRoot.getElementById('password').value;
+
+    
+    
+    if(username && password){
+
+        ajax
+        .post(url,formData)
+        
+        .then(response => {
+            this.errorMessage = null;
+            sessionStorage.setItem("username", username);
+            window.location.href = "/search";
+        })
+        .catch(error => {
+            this.errorMessage = "Invalid Username or Password";
+        });
+    }else{
+       console.log("field is empty"); 
+    }
+     
+}
+
+  
+
+  render() {
+    return html`
+   
+    <div class="big-image">
+        <div class="overlay">
+        <lion-form class="login-container" @submit=${this.authenticateUser} responsive>
                     <form class="login-form" @submit= ${(ev) =>ev.preventDefault()}>
                         <div class="cont">
                             <div class="title">
@@ -235,15 +239,14 @@ export class LoginComponent extends LocalizeMixin(LitElement){
                     </div>
                 </form>
             </lion-form>
-            </div>
+           
+        </div>
+    </div>
+                      
+    `;
+  }
 
-
-            
-        `;
-    }
-
+  
 }
 
 window.customElements.define('login-component', LoginComponent);
-
-
