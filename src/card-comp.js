14d212db5c27fc4a0d4391fab04f4f9ee6ac85dc
Padmashani <padmashani98@gmail.com>
2021-04-06 11:16:38 +0530

    Added edit and comments icons

--- src/card-comp.js
+++ src/card-comp.js
@@ -1,17 +1,22 @@
 import { LitElement, html, css } from "lit-element";
+import '@lion/icon/lion-icon.js';
+
+import editIcon from './images/edit-solid.js';
+import commentsIcon from './images/comments-dots-solid';
 
 class cardComp extends LitElement {
     static get properties() {
         return {
-            name: { type: String },
-            accountno: { type: String },
-            profileimg: { type: String },
+          name: { type: String },
+          accountno : { type: String },
+          profileimg : { type: String}
+          
         };
-    }
+      }
     constructor() {
         super();
-        this.accountno = "";
-        this.msg = "hi";
+        this.accountno='';
+        this.msg='hi';
     }
 
   static get styles(){
@@ -43,10 +48,23 @@ class cardComp extends LitElement {
           border:none;
         }
 
+        #editIcon{
+           margin-left: 10px;
+          font-size: 22px;
+        }
+
+        #commentsIcon{
+          font-size: 26px;
+        }
+
+        .commentsIcon{
+          border:none;
+          padding:0px;
+          display:flex;
+          justify-content:flex-end;
+        }
         img{
           border-radius:50%;
-          width:120px;
-          height:120px;
         }
         #accno {
             border:none;
@@ -57,59 +75,64 @@ class cardComp extends LitElement {
       `;
   }
 
-    updated() {
+  // updated(){
+  //     this.shadowRoot.getElementById('card-items').addEventListener('click',() => {
+  //       console.log("card clicked"); 
+  //       console.log(this.accountno);
+
+  //       sessionStorage.setItem('accno',this.accountno);
+
+  //       window.location.href='/custform/#'+this.accountno;
         
-        this.shadowRoot
-            .getElementById("card-items")
-            .addEventListener("click", () => {
-                console.log("card clicked");
-                console.log(this.accountno);
+  //       //const url = '/custform/'+this.accountno;
+  //       //window.location.href= '/custform/'+this.accountno;
+  //       //window.location.href='/details';
 
-                sessionStorage.setItem("accno", this.accountno);
 
-                const custAccno = {
-                  accno: this.accountno
-                }
+  //       //window.location.href='/details/'+this.accountno;
 
-                this.dispatchEvent(new CustomEvent('change-route',{bubbles: true, composed: true, detail: {
-                  route: '/custform',
-                  routeData: custAccno
+  //       /* const obj = { name:'Divya', lastname: 'Ram'};
+  //       window.location.href='/custform/?custaccno='+obj; */
+  //     });
+  //     //console.log(this.accountno);
+  // }
+
+
+
+editHandler(){
+  console.log("edit handler called");
+  const custAccno = {
+    accno: this.accountno
+  }
+
+  this.dispatchEvent(new CustomEvent('change-route',{bubbles: true, composed: true, detail: {
+    route: '/custform',
+    routeData: custAccno
+
+  }}));
+}
+
+commentsHandler(){
+  console.log("comments handler called");
+  // this.dispatchEvent(new CustomEvent('change-route',{bubbles: true, composed: true, detail: {
+  //     route: '/queryForm'
   
-              }}));
+  //   }}));
+}
+  render() {
+    return html`
+   
+		<div class="card-items" id="card-items"> 
+            <div class="commentsIcon"><lion-icon aria-label="comment section" .svg="${commentsIcon}" @click=${this.commentsHandler} id="commentsIcon"></lion-icon></div>
+            <div class="card-data"><img src=${"./images/" + this.profileimg}></div>
+            <div><b>${this.name}</b><lion-icon aria-label="Edit details" .svg="${editIcon}" @click=${this.editHandler} id="editIcon"></lion-icon></div>
+            <div id="accno">${this.accountno}</div>  
+        </div>       
+                      
+    `;
+  }
 
-               // window.location.href = "/custform/#" + this.accountno;
-
-                //const url = '/custform/'+this.accountno;
-                //window.location.href= '/custform/'+this.accountno;
-                //window.location.href='/details';
-
-                //window.location.href='/details/'+this.accountno;
-
-                /* const obj = { name:'Divya', lastname: 'Ram'};
-        window.location.href='/custform/?custaccno='+obj; */
-            });
-        //console.log(this.accountno);
-    }
-
-    //   cardClicked(){
-    //       //window.location.href='/details/'+this.accountno;
-    //       console.log("card clicked fun called");
-    //       console.log(this.accountno);
-    //       console.log(this.msg);
-    //       //window.location.href='/details';
-    //   }
-
-    render() {
-        return html`
-            <div class="card-items" id="card-items">
-                <div class="card-data">
-                    <img src=${"./images/" + this.profileimg} />
-                </div>
-                <div><b>${this.name}</b></div>
-                <div id="accno">${this.accountno}</div>
-            </div>
-        `;
-    }
+  
 }
 
 customElements.define("card-comp", cardComp);
